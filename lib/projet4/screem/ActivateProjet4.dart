057fca00285a23e00b3f399d8ccf587a09496db7
pokou2020 <lepnkouakou@gmail.com>
2024-05-29 17:26:52 +0000

    bon

--- lib/projet4/screem/ActivateProjet4.dart
+++ lib/projet4/screem/ActivateProjet4.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:pinput/pinput.dart';
+import 'package:projet1/projet4/screem/ActivationEtape2.dart';
 import 'package:sizer/sizer.dart';
 
 class ActivateProjet4 extends StatefulWidget {
@@ -17,7 +17,9 @@ class _ActivateProjet4State extends State<ActivateProjet4> {
   /// this widget will be use for each digit
   Widget numButton(int number) {
     return Padding(
-      padding: const EdgeInsets.only(top: 16),
+      padding: const EdgeInsets.only(
+        top: 16,
+      ),
       child: TextButton(
         onPressed: () {
           setState(() {
@@ -41,118 +43,176 @@ class _ActivateProjet4State extends State<ActivateProjet4> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: ListView(
-          padding: const EdgeInsets.symmetric(horizontal: 24),
-          physics: const BouncingScrollPhysics(),
-          children: [
-            const  Text("Enter code",
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold
-          ),
-           ),
-           SizedBox(height: 2.h,),
-          const Text("A 4-digit code was sent to j*******ide@gmail.com.",
-           
-            ),
-        
-          const  Text("Enter the code to continue.",
-          
-            ),
-      SizedBox(height: 2.h,),
-            Text("Send to my phone number instead",
-            style: TextStyle(color:Colors.orange[900],
-            fontSize: 16,
-            decoration: TextDecoration.underline,
-            decorationColor:Colors.orange[900],
-            
-            ),
-            ),
-            const SizedBox(height: 50),
 
-            /// pin code area
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: List.generate(
-                4,
-                (index) {
-                  return Container(
-                    margin: const EdgeInsets.all(6.0),
-                    width:  65 ,
-                    height:  65,
-                    decoration: BoxDecoration(
-                       border: Border.all(color: Colors.orange),
-                      borderRadius: BorderRadius.circular(15),
-                      color: index < enteredPin.length
-                          ? isPinVisible
-                              ? Colors.orange[100]
-                              :Colors.orange[100]
-                          : Colors.orange[100]
+      appBar: AppBar(),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.only( right: 5.w, left: 5.w),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                flex: 3,
+                  child: Container(
+                
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                  
+                    const Text(
+                      "Enter code",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                     ),
-                    child: isPinVisible && index < enteredPin.length
-                        ? Center(
-                            child: Text(
-                              enteredPin[index],
-                              style: const TextStyle(
-                                fontSize: 17,
+                    SizedBox(
+                      height: 2.h,
+                    ),
+                    const Text(
+                      "A 4-digit code was sent to j*******ide@gmail.com.",
+                    ),
+                    const Text(
+                      "Enter the code to continue.",
+                    ),
+                    SizedBox(
+                      height: 2.h,
+                    ),
+                    Text(
+                      "Send to my phone number instead",
+                      style: TextStyle(
+                        color: Colors.orange[900],
+                        fontSize: 16,
+                        decoration: TextDecoration.underline,
+                        decorationColor: Colors.orange[900],
+                      ),
+                    ),
+                  ],
+                ),
+              )),
+
+              /// Code pin
+
+              Expanded(
+                flex: 4,
+                  child: Container(
+              
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: List.generate(
+                        4,
+                        (index) {
+                          return Container(
+                            margin: const EdgeInsets.all(6.0),
+                            width: 65,
+                            height: 65,
+                            decoration: BoxDecoration(
+                                border: Border.all(color: Colors.orange),
+                                borderRadius: BorderRadius.circular(15),
+                                color: index < enteredPin.length
+                                    ? isPinVisible
+                                        ? Colors.orange[100]
+                                        : Colors.orange[100]
+                                    : Colors.orange[100]),
+                            child: isPinVisible && index < enteredPin.length
+                                ? Center(
+                                    child: Text(
+                                      enteredPin[index],
+                                      style: const TextStyle(
+                                        fontSize: 17,
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.w700,
+                                      ),
+                                    ),
+                                  )
+                                : null,
+                          );
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                      height: 3.h,
+                    ),
+                    Center(child: Text("Didn't receive a code? Resend in")),
+                    Center(child: Text("05:00")),
+                    SizedBox(
+                      height: 3.h,
+                    ),
+                  ],
+                ),
+              )),
+
+              Expanded(
+                flex: 7,
+                  child: Container(
+                child: Column(
+                   mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    for (var i = 0; i < 3; i++)
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 24),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: List.generate(
+                            3,
+                            (index) => numButton(1 + 3 * i + index),
+                          ).toList(),
+                        ),
+                      ),
+
+                    /// 0 digit with back remove
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 24),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          InkWell(
+                            onTap: () {
+                              setState(
+                                () {
+                                  if (enteredPin.isNotEmpty) {
+                                    enteredPin = enteredPin.substring(
+                                        0, enteredPin.length - 1);
+                                  }
+                                },
+                              );
+                            },
+                            child: Container(
+                              width: 50,
+                              child: const Icon(
+                                Icons.backspace_outlined,
+                                color: Colors.black,
+                                size: 24,
+                              ),
+                            ),
+                          ),
+                          numButton(0),
+                          InkWell(
+                            onTap: () {
+                            Navigator.of(context).push(MaterialPageRoute(builder: (context)=>ActivationEtape2()));
+                            },
+                            child: Container(
+                              height: 50,
+                              width: 70,
+                              decoration: BoxDecoration(
+                                  color: Colors.black,
+                                  borderRadius: BorderRadius.circular(30)),
+                              child: const Icon(
+                                Icons.arrow_forward,
                                 color: Colors.white,
-                                fontWeight: FontWeight.w700,
+                                size: 24,
                               ),
                             ),
                           )
-                        : null,
-                  );
-                },
-              ),
-            ),
-
-          
-            for (var i = 0; i < 3; i++)
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 24),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: List.generate(
-                    3,
-                    (index) => numButton(1 + 3 * i + index),
-                  ).toList(),
-                ),
-              ),
-
-            /// 0 digit with back remove
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const TextButton(onPressed: null, child: SizedBox()),
-                  numButton(0),
-                  TextButton(
-                    onPressed: () {
-                      setState(
-                        () {
-                          if (enteredPin.isNotEmpty) {
-                            enteredPin =
-                                enteredPin.substring(0, enteredPin.length - 1);
-                          }
-                        },
-                      );
-                    },
-                    child: const Icon(
-                      Icons.backspace,
-                      color: Colors.black,
-                      size: 24,
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-
-
-           
-          ],
+                  ],
+                ),
+              ))
+            ],
+          ),
         ),
       ),
     );
